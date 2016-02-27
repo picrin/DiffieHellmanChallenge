79199b6ff21bdee5a526a678dae5ddb5ab1950ed
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-02-27 17:49:05 +0000

    add more prime numbers.

--- verify.sh
+++ verify.sh
@@ -1,1 +1,7 @@
-python discreteLog.py smallPrime 42 | xargs python checkAnswer.py smallPrime 
+python discreteLogEfficient.py smallPrime 42 | xargs python checkAnswer.py smallPrime
+
+python discreteLogEfficient.py mediumPrime 111 | xargs python checkAnswer.py mediumPrime
+
+python discreteLogEfficient.py bigPrime 89 | xargs python checkAnswer.py bigPrime
+
+python discreteLogEfficient.py humongousPrime 5885135 | xargs python checkAnswer.py humongousPrime
